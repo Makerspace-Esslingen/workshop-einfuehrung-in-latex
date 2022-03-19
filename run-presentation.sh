8cd7c4bd293697f3ae20e444f141e93c880a388d
Andre Herbst <moormaster@gmx.net>
2022-03-19 13:59:38 +0100

    be sure to kill webserver when ./run_presentation.sh was stopped with ctrl+c

--- run-presentation.sh
+++ run-presentation.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 python -m http.server 8080 &
+echo started webserver: pid $( jobs -p )
 xdg-open http://localhost:8080/presentation.html
+trap 'echo "Killing webserver with pid $( jobs -p )..."; kill $( jobs -p )' SIGINT
 wait
+
